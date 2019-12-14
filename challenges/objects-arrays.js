3e8577c9d6d04df26e44109545b8e3e0c6d0aef0
5f1839842f2fb <57242175+5f1839842f2fb@users.noreply.github.com>
2019-12-13 21:25:55 -0500

    Update objects-arrays.js

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -5,42 +5,41 @@
   Use this pattern to create your objects: 
   object name, diet, weight, length, period
 */
-
+function dinomaker(name, diet, weight, length, period) {
+  return dino = {
+    name: name,
+    diet: diet,
+    weight: weight,
+    length: length,
+    period: period,
+    roar: function() {
+      return "RAWERSRARARWERSARARARRRR!";
+    }
+  }
+}
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
-const tyrannosaurus = {
-  name: 'tyrannosaurus',
-  diet: 'carniverous',
-  weight: '7000kg',
-  length: '12',
-  period: 'Late Cretaceous'
-}
+const trex = dinomaker('tyrannosaurus', 'carnivorous', '7000kg', '12m', 'Late Cretaceous');
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
-const stegosaurus = {
-  name: 'stegosaurus',
-  diet: 'herbivorous',
-  weight: '7000kg',
-  length: '12',
-  period: 'Late Cretaceous'
-}
+const stegosaurus = dinomaker('stegosaurus', 'herbivorous', '2000kg', '9m', 'Late Jurassic');
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
-
+const velociraptor = dinomaker('veliciraptor', 'carnivorous', '15kg', '1.8m', 'Late Cretaceous');
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(trex.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(velociraptor.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(stegosaurus.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(trex.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(trex.roar());
 
 
 // ==== Arrays ====
@@ -64,6 +63,10 @@ const graduates = [
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
+for (i = 0; i < graduates.length; i++) {
+  universities.push(graduates[i].university);
+}
+universities.sort();
 console.log(universities);
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
